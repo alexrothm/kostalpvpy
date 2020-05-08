3ec8d42e83399a90dae914bcc60516b3b53da06c
Alexander Rotherm <a.rotherm@live.de>
2020-05-08 18:55:45 +0200

    added functionality to the dashboard

--- kostalpvpy/home/controller.py
+++ kostalpvpy/home/controller.py
@@ -1,15 +1,27 @@
 from flask import Blueprint, render_template
 
-from kostalpvpy.pvdata.models import PVData
+from kostalpvpy.pvdata.helper import (get_current_values, get_last_year_energy, get_max_energy_last_seven_days)
 
 bp_home = Blueprint("home", __name__)
 
 
 @bp_home.route("/")
 def home():
-    return render_template("home/home.html", pvdata=None)
+    # photovoltaic data
+    pv = get_current_values()
+    daily_energy = pv.daily_energy
+    total_energy = pv.total_energy
 
+    max_daily_energy_last_seven_days = get_max_energy_last_seven_days()
 
-@bp_home.route("/test2")
-def test():
-    return render_template("home/sample_solar.html")
+    last_year_energy = get_last_year_energy()
+    current_year_energy = total_energy
+
+    if hasattr(last_year_energy, "total_energy"):
+        current_year_energy = total_energy - last_year_energy.total_energy
+
+    return render_template("home/home.html",
+                           daily_energy=daily_energy,
+                           max_daily_energy_last_seven_days=max_daily_energy_last_seven_days,
+                           current_year_energy=current_year_energy,
+                           total_energy=total_energy)
--- kostalpvpy/pvdata/helper.py
+++ kostalpvpy/pvdata/helper.py
@@ -16,12 +16,12 @@ def get_max_energy_last_seven_days():
     """
     :return: the maximum energy produced in the last seven days
     """
-    return (PVData.query.whith_entities(func.max(PVData.daily_energy).label('max_daily_energy'))
+    return (PVData.query.with_entities(func.max(PVData.daily_energy).label('max_daily_energy'))
             .filter(PVData.created_at >= (datetime.now() - timedelta(days=7))).first().max_daily_energy)
 
 
 def get_last_year_energy():
     current_year = datetime.now().year
-    return (PVData.query.whith_entities(PVData.total_energy)
+    return (PVData.query.with_entities(PVData.total_energy)
             .filter(func.strftime('%Y', PVData.created_at) == str(current_year - 1))
             .order_by(PVData.id.desc()).first())
